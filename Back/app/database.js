c10f1fe8c8c9f59805710dc5f4bb8309a797830b
Nicolas-B06 <nicolas.bernard@nextsourcia.com>
2022-05-06 12:51:09 +0200

    [Update] Create item model

--- Back/app/database.js
+++ Back/app/database.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.PG_URL, {
+    dialect: 'postgres',
+});
      
 const { Client } = require('pg');
 const db = new Client({connectionString: process.env.DATABASE_URL,
@@ -8,4 +10,4 @@ const db = new Client({connectionString: process.env.DATABASE_URL,
 db.connect()  
 
 
-module.exports = db;
+module.exports = sequelize;
